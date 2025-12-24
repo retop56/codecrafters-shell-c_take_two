cbe675d1bbf3a5f11ea0d350a2372c6606876edf
Big Homie KG <retop56@gmail.com>
2025-12-23 22:51:16 -0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,33 +1,57 @@
 #include "arg_obj_def.h"
 #include "argparser.h"
-#include "cc_shell.h"
 #include "handle_commands.h"
 #include <readline/readline.h>
 #include <readline/history.h>
-/*char input[BUFF_LENGTH];*/
+#include <time.h>
 struct arg_obj *ao;
+
+char *possible_completion_options[] = {
+  "echo",
+  "exit",
+  NULL
+};
+
+char * command_name_generator(const char *text, int state) {
+  static int list_index, len;
+  char * name;
+
+  if (!state) {
+    list_index = 0;
+    len = strlen(text);
+  }
+
+  while ((name = possible_completion_options[list_index++])) {
+    if (strncmp(name, text, len) == 0) {
+      return strdup(name);
+    }
+  }
+
+  return NULL;
+}
+
+char **completion_func(const char *text, int start, int end) {
+  rl_attempted_completion_over = 1;
+  return rl_completion_matches(text, command_name_generator);
+};
+
 
 int main() {
   rl_bind_key('\t', rl_complete);
+  rl_attempted_completion_function = completion_func;
+
   ao = create_arg_obj();
+  char *input;
   while (true) {
-    /*printf("$ ");*/
-    /*fflush(NULL);*/
     // Wait for user input
-    
-    /*if (fgets(input, BUFF_LENGTH, stdin) == NULL) {*/
-      /*continue;*/
-    /*}*/
-    char *input = readline("$ ");
+    input = readline("$ ");
     if (!input) break;
     // Replace \n at end of string with null
     int len_of_input = strlen(input);
     if (len_of_input > 0 && input[len_of_input - 1] == '\n') {
       input[len_of_input - 1] = '\0';
     }
-    /*ao->input = (char *)&input;*/
     ao->input = input;
-    /*ao->curr_char = (char *)&input;*/
     ao->curr_char = input;
     add_args();
     if (ao->redir_type != INITIAL_VAL) {
@@ -48,5 +72,6 @@ int main() {
     clear_args();
   }
   free(ao);
+  free(input);
   return 0;
 }
